126700d031e4c2a2631806e1bb7e0891d181268f
admin <admin@qq.com>
2019-03-15 18:04:14 +0800

    注释数据库配置

--- confweb/settings.py
+++ confweb/settings.py
@@ -86,10 +86,10 @@ DATABASES = {
          'PASSWORD':'confd123',
          'HOST':'127.0.0.1',
          'PORT':'3306',
-         'OPTIONS':{
-            'init_command':"SET sql_mode='STRICT_TRANS_TABLES'",
-            'charset':'utf8mb4',
-        },
+        #  'OPTIONS':{
+        #     'init_command':"SET sql_mode='STRICT_TRANS_TABLES'",
+        #     'charset':'utf8mb4',
+        # },
      }
 }
 
